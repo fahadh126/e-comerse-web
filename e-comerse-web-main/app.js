8e3eb18502510ee489294fe798d0087d12d175b6
fahadh <fahadsiddique535@gmail.com>
2024-03-25 13:38:31 +0530

    update

---
+++ e-comerse-web-main/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app =express();
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const cors =require('cors');
+require ('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler =require('./helpers/error-handler')
+
+app.use(cors());
+app.options('*',cors())
+ 
+//middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(authJwt())
+app.use('/public/uploads',express,static(__dirname + '/public/uploads'))
+app.use (errorHandler)
+
+//Routers
+const categoriesRoutes =require('./routers/categories');
+const  productsRouters= require ('./routers/products');
+const usersRouters = require ('./routers/users');
+const ordersRouters = require ('./routers/orders');
+
+
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`,categoriesRoutes);
+app.use(`${api}/products`,productsRouters);
+app.use(`${api}/users`,usersRouters);
+app.use(`${api}/orders`,ordersRouters);
+
+//database connection
+ mongoose.connect(process.env.CONNECTION_STRING)
+ .then(()=>{
+    console.log('database connection is ready...');
+ })
+ .catch((err)=>{
+    console.log(err);
+ });
+
+
+
+//server
+ app.listen(3000, ()=>{
+    console.log('server runs in http://localhost:3000');
+ })
